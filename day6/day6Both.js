d403e19192adcd9d875e117d8ceb14a2667e7b58
Nicke Broström <54627464+Nickztar@users.noreply.github.com>
2019-12-06 10:17:43 +0100

    Fix

--- day6/day6Both.js
+++ day6/day6Both.js
@@ -1,7 +1,6 @@
 const { readFileSync } = require('fs');
 const re = new RegExp('\r', 'g');
 const input = readFileSync("./day6Input.txt", "utf8").replace(re, '').split('\n').map(s => s.split(')'));
-console.log(input);
 console.time("RuntimeP1");
 console.time("RuntimeP2");
 //Maps out all of the points and their parents
@@ -26,7 +25,7 @@ for(const key of Object.keys(nodes)){
     }
 }
 console.log(`Part 1: ${orbitCheck}`);
-console.timeEnd("RuntimeP2");
+console.timeEnd("RuntimeP1");
 
 
 //Walk from both directions. Walk from me and santa and count the steps it took from each directon
@@ -47,3 +46,9 @@ while(me.parent){
     me = me.parent;
     fromMe++;
 }
+/* 
+Part 1: 147807
+RuntimeP1: 25.881ms
+Part 2: 229
+RuntimeP2: 32.357ms
+*/
